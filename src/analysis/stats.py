f2361a48cf97656a0fa80e105ecb6a4a88f4471b
Triyan0 <sweidana05@gmail.com>
2026-04-15 01:58:13 +0100

    stats.py | def assign_tournament_weight | completed and initialised the function that categorises each weighting for each tournament for better prediction in later models

--- src/analysis/stats.py
+++ src/analysis/stats.py
@@ -72,3 +72,41 @@ def get_team_matches(df, team):
     team_matches = df[(df["home_team"] == team) | (df["away_team"] == team)] # Filter matches where the team is either home or away
     return team_matches # Return the filtered DataFrame
 
+
+
+def filter_by_year(df):
+    """Filters the DataFrame to include only matches from year 2014 onwards.
+    
+    Args:
+        df (pd.DataFrame): The DataFrame to filter.
+    
+    Returns:
+        pd.DataFrame: The filtered DataFrame with matches from year 2014 onwards.
+    """
+    df['date'] = pd.to_datetime(df['date']) # Convert the 'date' column to datetime
+
+    filtered_df = df[df['date'].dt.year >=  STATS_FROM_2014] # Filter matches from year 2014 onwards
+    return filtered_df # Return the filtered DataFrame
+
+
+def assign_tournament_weight(df):
+    """Assigns a weight to each match based on the tournament type.
+    
+    Args:
+        df (pd.DataFrame): The DataFrame containing match data.
+
+    Returns:
+        pd.DataFrame: The DataFrame with a new 'tournament_weight' column.
+    """
+    weight_category_3 = df[(df['tournament'] == "FIFA World Cup") | (df['tournament'] == "UEFA Euro") | (df['tournament'] == "Copa América") | (df['tournament'] == "Africa Cup of Nations")] # Define category 3 tournaments
+    weight_category_2 = df[(df['tournament'] == "FIFA World Cup qualification") | (df['tournament'] == "UEFA Euro qualification") | (df['tournament'] == "UEFA Nations League")] # Define category 2 tournaments
+    weight_category_1 = df[df['tournament'] == "Friendly"] # Define category 1 tournaments
+
+    weight_category_3['tournament_weight'] = 3 # Assign weight 3 to category 3 tournaments
+    weight_category_2['tournament_weight'] = 2 # Assign weight 2 to category 2 tournaments
+    weight_category_1['tournament_weight'] = 1 # Assign weight 1 to category 1 tournaments
+
+    weighted_df = pd.concat([weight_category_3, weight_category_2, weight_category_1]) # Combine the weighted categories back into a single DataFrame
+
+
+    return weighted_df # Return the DataFrame with tournament weights assigned
