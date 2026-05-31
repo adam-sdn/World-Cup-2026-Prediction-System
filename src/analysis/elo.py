dbfb67e4c743a7aa328ac51e1ba252796919285c
Triyan0 <sweidana05@gmail.com>
2026-05-31 09:51:58 +0100

    Enhance elo.py documentation by adding ELO Rating formula and clarifying output details

--- src/analysis/elo.py
+++ src/analysis/elo.py
@@ -6,6 +6,10 @@ elo.py
 Gives an updated ELO Rating for each team based on the match results. This is used as a feature in the predictor model.
 Gets data from the results_clean.csv file from data\processed created by export.py
 
+
+ELO Rating Formula : 
+New Rating = Old Rating + K × (Actual Result - Expected Result)
+
 Output: the following is generated :
     
     elo_ratings.csv - A CSV file containing the ELO ratings for each team, saved in data/processed/elo_ratings.csv 
@@ -19,3 +23,5 @@ import pandas as pd
 import os
 from src.data.export import load_processed_match_results
 
+
+ELO_BASE_RATING = 1000
